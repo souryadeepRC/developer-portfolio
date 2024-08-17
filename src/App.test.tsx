14a9b8ddbfa5cd61e03eb5b7cdb40f640cad4a68
souryadeepRC <thecreativeworldforall@gmail.com>
2024-08-18 00:05:15 +0530

    modify css to scss- format code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "src/App";
 
-test('renders Developer portfolio header', () => {
+test("renders Developer portfolio header", () => {
   render(<App />);
   const linkElement = screen.getByText(/Developer portfolio/i);
   expect(linkElement).toBeInTheDocument();
